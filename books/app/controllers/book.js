7af8231272977b7609ef1df8ef4960f32f7fbd82
Denperidge <denperidge@gmail.com>
2023-01-04 17:24:06 +0100

    Moved code to controller
    Thanks to @Riadabd for catching my goof

--- books/app/controllers/book.js
+++ books/app/controllers/book.js
@@ -1,3 +1,31 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 
-export default class BookController extends Controller {}
+export default class BookController extends Controller {
+    @tracked newTitle = '';
+    @tracked newIsbn = '';
+
+    @service store;
+
+    @action
+    createBook(event) {
+        event.preventDefault();
+        // Create the new book
+        const book = this.store.createRecord('book', {
+            title: this.newTitle,
+            isbn: this.newIsbn
+        });
+        book.save();
+        // Clear the input fields
+        this.newTitle = '';
+        this.newIsbn = '';
+    }
+
+    @action
+    removeBook( book, event ) {
+        event.preventDefault();
+        book.destroyRecord();
+    }
+}
